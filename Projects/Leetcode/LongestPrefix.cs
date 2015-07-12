fee48689c3d3ab98df6e81a01665cb19da814161
crysisfair <crysisfair@qq.com>
2015-07-13 01:25:30 +0800

    添加Longest Prefix

---
+++ Projects/Leetcode/LongestPrefix.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class LongestPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            List<char> chars = new List<char>();
+            if(strs.Count<string>() > 0)
+            {
+                chars.AddRange(strs[0].ToCharArray());
+            }
+            bool notAllowAdd = false;
+            for(int s = 1; s < strs.Length; s++ )
+            {
+                char[] strChars = strs[s].ToCharArray();
+                if(strChars.Length < chars.Count)
+                {
+                    chars.RemoveRange(strChars.Length, chars.Count - strChars.Length);
+                    notAllowAdd = true;
+                }
+                for(int i = 0; i < strs[s].Length; i++ )
+                {
+                    if (i >= chars.Count) break;
+                    else if(strChars[i] != chars[i])
+                    {
+                        chars.RemoveRange(i, chars.Count - i);
+                        notAllowAdd = true;
+                        break;
+                    }
+                    else if (strChars[i] == chars[i]) continue;
+                    else if(notAllowAdd == false)
+                    {
+                        chars.Add(strChars[i]);
+                    }
+                    else { break; }
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in chars) sb.Append(c);
+            return sb.ToString();
+        }
+    }
+}
---
+++ Projects/LeetcodeUnitTest/LongestPrefixTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode;
+
+namespace LeetcodeUnitTest
+{
+    [TestClass]
+    public class LongestPrefixTest
+    {
+        [TestMethod]
+        public void LongestPrefixTestNormal()
+        {
+            LongestPrefix l = new LongestPrefix();
+            string[] case2 = new string[] { "aaaaaa", "aaa", "aaaaaaaa", "aaaaaaaa", "aaaaaaaaaaaaaa", "aaaaaaaaaaaaaaa", "aaa", "aaaa"};
+            Assert.AreEqual("aaa", l.LongestCommonPrefix(case2));
+            string[] case1 = new string[] { "casetestok", "caset345", "caset", "caset3,.df", "caset", "casetldf"};
+            Assert.AreEqual("caset", l.LongestCommonPrefix(case1));
+        }
+    }
+}
