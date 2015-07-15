5dd510db52d59561fd8be4af53d43901282761b4
crysisfair <crysisfair@qq.com>
2015-07-15 10:35:20 +0800

    添加BestTimeToBuyAndSellII

--- Projects/Leetcode/BestTimeToBuyAnsSell.cs
+++ Projects/Leetcode/BestTimeToBuyAnsSell.cs
@@ -1,9 +1,4 @@
-﻿/*
- * Say you have an array for which the ith element is the price of a given stock on day i.
- * 
- * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
- */
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace Leetcode
 {
+    /*
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     * 
+     * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+     */
     public class BestTimeToBuyAnsSell
     {
         public int MaxProfit(int[] prices)
@@ -39,6 +39,30 @@ namespace Leetcode
                 }
             }
             return maxProfit;
+        }
+    }
+
+    /*
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+
+     * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, 
+     * buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions 
+     * at the same time (ie, you must sell the stock before you buy again).
+     */
+    public class BestTimeToBuyAndSellII
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length <= 1) return 0;
+            int profit = 0;
+            for(int i= 1; i < prices.Length; i++)
+            {
+                if(prices[i] > prices[i - 1])
+                {
+                    profit += prices[i] - prices[i - 1];
+                }
+            }
+            return profit;
         }
     }
 }
