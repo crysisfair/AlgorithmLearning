79759e6521efe7b308cdc352112d97e120095fc7
crysisfair <crysisfair@qq.com>
2015-07-17 21:49:45 +0800

    添加SquareRoot

---
+++ Projects/Leetcode/Square.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Implement int sqrt(int x).
+ * 
+ * Compute and return the square root of 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class Square
+    {
+        public int MySqrt(int x)
+        {
+            if (x < 0) return 0;
+            else if (x == 1) return 1;
+            int mid = 1;
+            int low = 1;
+            int high = x;
+            int calc = 0;
+            while(low <= high)
+            {
+                int temp = (high - low) >> 2;
+                mid = low + temp;
+                calc = x / mid;
+                if(calc == mid)
+                {
+                    return mid;
+                }
+                else if(calc < mid)
+                {
+                    high = mid - 1;
+                }
+                else if(calc > mid)
+                {
+                    low = mid + 1;
+                }
+            }
+            return high; // 返回的high值为小于或等于low值，即正好刚等于sqrt(x)向下取整
+        }
+    }
+}
---
+++ Projects/LeetcodeUnitTest/SquareTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode;
+
+namespace LeetcodeUnitTest
+{
+    [TestClass]
+    public class SquareTest
+    {
+        [TestMethod]
+        public void SquareTestNormal()
+        {
+            Square s = new Square();
+            int case1 = 2;
+            int case2 = 64;
+            int case3 = 65;
+            int case4 = 63;
+            int case5 = 512;
+            int case6 = 300;
+            int case7 = 1000;
+            Assert.AreEqual((int)Math.Sqrt(case1), s.MySqrt(case1));
+            Assert.AreEqual((int)Math.Sqrt(case2), s.MySqrt(case2));
+            Assert.AreEqual((int)Math.Sqrt(case3), s.MySqrt(case3));
+            Assert.AreEqual((int)Math.Sqrt(case4), s.MySqrt(case4));
+            Assert.AreEqual((int)Math.Sqrt(case5), s.MySqrt(case5));
+            Assert.AreEqual((int)Math.Sqrt(case6), s.MySqrt(case6));
+            Assert.AreEqual((int)Math.Sqrt(case7), s.MySqrt(case7));
+        }
+    }
+}
