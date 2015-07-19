6177189d259a4bbf5ef8a9547ab55d94fd4cee37
crysisfair <crysisfair@qq.com>
2015-07-19 23:35:37 +0800

    添加TwoSum

---
+++ Projects/Leetcode/TwoSum.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Given an array of integers, find two numbers such that they add up to a specific target number.
+
+ * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+ * 
+ * You may assume that each input would have exactly one solution.
+ * 
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=1, index2=2 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class CalcTwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            if (nums.Length <= 1) return new int[] { 0, 0 };
+            Dictionary<int, List<int>> t = new Dictionary<int, List<int>>();
+            List<int> ns = new List<int>();
+            ns.AddRange(nums);
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(t.ContainsKey(nums[i]))
+                {
+                    t[nums[i]].Add(i + 1);
+                }
+                else
+                {
+                    t.Add(nums[i], new List<int>() { i + 1 });
+                }
+            }
+            ns.Sort();
+            int calc = 0;
+            for(int i = 0; i < ns.Count; i++ )
+            {
+                int j = i + 1;
+                while( j < ns.Count)
+                {
+                    calc = ns[i] + ns[j];
+                    if(calc < target)
+                    {
+                        j += 1;
+                    }
+                    else if(calc == target)
+                    {
+                        int index1;
+                        int index2;
+                        if(ns[i] == ns[j])
+                        {
+                            index1 = t[ns[i]][0];
+                            index2 = t[ns[j]][1];
+                        }
+                        else
+                        {
+                            index1 = t[ns[i]][0];
+                            index2 = t[ns[j]][0];
+                        }
+                        if(index1 > index2)
+                        {
+                            return new int[] { index2, index1 };
+                        }
+                        return new int[] { index1, index2 };
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return new int[2] { 0, 0 };
+        }
+    }
+}
---
+++ Projects/LeetcodeUnitTest/TwoSumTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode;
+
+namespace LeetcodeUnitTest
+{
+    [TestClass]
+    public class TwoSumTest
+    {
+        bool CheckResult(int[] expt, int[] actual)
+        {
+            if (expt[0] == actual[0] && expt[1] == actual[1]) return true;
+            return false;
+        }
+
+        [TestMethod]
+        public void TwoSumTestNormalCase()
+        {
+            CalcTwoSum t = new CalcTwoSum();
+            int[] case1 = new int[] { 7, 11, 15, 2 };
+            int target1 = 9;
+            int[] case1Res = new int[] { 1, 4 };
+            int[] case2 = new int[] { 2, 7, 0, 15 };
+            int target2 = 7;
+            int[] case2Res = new int[] { 2, 3 };
+            int[] case3 = new int[] { 2, 0, 0, 15 };
+            int target3 = 0;
+            int[] case3Res = new int[] { 2, 3 };
+            Assert.IsTrue(CheckResult(case1Res, t.TwoSum(case1, target1)));
+            Assert.IsTrue(CheckResult(case2Res, t.TwoSum(case2, target2)));
+            Assert.IsTrue(CheckResult(case3Res, t.TwoSum(case3, target3)));
+        }
+    }
+}
