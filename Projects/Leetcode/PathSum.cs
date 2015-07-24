54ee564fe3fda0547bec059206dc5dbdd7379a55
crysisfair <crysisfair@qq.com>
2015-07-24 21:16:11 +0800

    未完成PathSumII

--- Projects/Leetcode/PathSum.cs
+++ Projects/Leetcode/PathSum.cs
@@ -167,4 +167,44 @@ namespace Leetcode
             return Search(root, sum, 0);
         }
     }
+
+    /*
+     * Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum. 
+     * For example:
+     *  Given the below binary tree and sum = 22,               5
+     *              / \
+     *             4   8
+     *            /   / \
+     *           11  13  4
+     *          /  \    / \
+     *         7    2  5   1
+     * 
+     * 
+     * return
+     * 
+     * [
+     *    [5,4,11,2],
+     *    [5,8,4,5]
+     * ]
+     */
+    public class PathSumII
+    {
+        void Search(ref List<IList<int>> res, PathSumLinkList.TreeNode node, int sum, int currentSum)
+        {
+            if (node == null) return;
+
+            if (node.val < 0) return;
+
+            if(node.right == null && node.left == null)
+            {
+                if(currentSum + node.val == sum)
+                {
+                }
+            }
+        }
+        public IList<IList<int>> PathSum(PathSumLinkList.TreeNode root, int sum)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+        }
+    }
 }
